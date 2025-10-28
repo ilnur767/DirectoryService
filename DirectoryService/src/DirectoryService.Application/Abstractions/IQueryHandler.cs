da7b8d8fce66c322a715c2c3ce687d0768a65203
ilnur767 <email@email.com>
2025-10-28 23:21:31 +0500

    add validators

--- DirectoryService/src/DirectoryService.Application/Abstractions/IQueryHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Abstractions/IQueryHandler.cs
@@ -1,8 +1,11 @@
+using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Shared.Errors;
+
 namespace DirectoryService.Application.Abstractions;
 
 public interface IQueryHandler<TResponse, in TQuery> where TQuery : IQuery
 {
-    public Task<TResponse> Handle(TQuery query, CancellationToken cancellationToken);
+    public Task<Result<TResponse, ErrorList>> Handle(TQuery query, CancellationToken cancellationToken);
 }
 
 public interface IQuery;
---
+++ DirectoryService/src/DirectoryService.Application/Commands/AddLocation/CreateLocationValidator.cs
@@ -0,0 +1,15 @@
+﻿using DirectoryService.Application.Validation;
+using DirectoryService.Domain.ValueObjects;
+using FluentValidation;
+
+namespace DirectoryService.Application.Commands.AddLocation;
+
+public class CreateLocationValidator : AbstractValidator<AddLocationCommand>
+{
+    public CreateLocationValidator()
+    {
+        RuleFor(x => x.Name).MustBeValueObject(LocationName.Create);
+        RuleFor(x => x.Address).MustBeValueObject(Address.Create);
+        RuleFor(x => x.TimeZone).MustBeValueObject(Timezone.Create);
+    }
+}
