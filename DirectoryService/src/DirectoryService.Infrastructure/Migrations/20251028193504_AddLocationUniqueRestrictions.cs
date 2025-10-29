afdc965d76ea959da6d43f3dab00edb5d371b13a
ilnur767 <email@email.com>
2025-10-29 11:56:12 +0500

    location unique restriction

---
+++ DirectoryService/src/DirectoryService.Infrastructure/Migrations/20251028193504_AddLocationUniqueRestrictions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationUniqueRestrictions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_locations_address",
+                table: "locations",
+                column: "address",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_locations_name",
+                table: "locations",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_locations_address",
+                table: "locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_locations_name",
+                table: "locations");
+        }
+    }
+}
